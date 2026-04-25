64a0bedb7f59103e478d5e2e2e659ded622f5ac6
Maham Khursheed <mahamkhursheed67@gmail.com>
2026-04-25 23:09:02 +0500

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,10 @@
+const number1 = Number(prompt("Enter first number: "));
+const number2 = Number(prompt("Enter second number: "));
+
+const sum = number1 + number2;
+const difference = number1 - number2;
+const product = number1 * number2;
+const quotient = number1 / number2;
+
+alert("The numbers are " + number1 + " and " + number2 + ". Their Sum is " + sum + ", difference is " + difference + ", product is " + product + " and quotient is " + quotient + ".");
+console.log("The numbers are " + number1 + " and " + number2 + ". Their Sum is " + sum + ", difference is " + difference + ", product is " + product + " and quotient is " + quotient + ".");
