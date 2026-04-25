b9b201300e98bdf95e42d2c8c320597e9928dfa7
Maham Khursheed <mahamkhursheed67@gmail.com>
2026-04-25 23:10:20 +0500

    Add files via upload

---
+++ Guess Game/script.js
@@ -0,0 +1,11 @@
+const num=674;
+const guess= prompt("Guess:")
+
+if(num==guess){
+    alert("You win")
+    console.log("You win")
+}
+else{
+    alert("Try again")
+    console.log("Try again")
+}
